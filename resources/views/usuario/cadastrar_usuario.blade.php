4fe4c207a4073b24c4a8fa845a6a658ca46d24df
Mario Otavio <devsgmm@gmail.com>
2023-06-14 21:21:06 -0300

    primeiro commit

--- resources/views/usuario/cadastrar_usuario.blade.php
+++ resources/views/usuario/cadastrar_usuario.blade.php
@@ -415,7 +415,7 @@
                     <!--  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"> 
                     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
                     <div class="invalid-feedback">
-                        Digite um nome
+                        Digite um nome!
                     </div>
                 </div>
                 <div class="mb-3">
