3b8647cc05cbf587ea963297320e407fb123a205
MuriloMafra <murilommafra@gmail.com>
2023-06-26 21:27:19 -0300

    Requisições

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -11,9 +11,15 @@ class LoginRequest extends FormRequest
      *
      * @return bool
      */
+    public function getCredentials(){
+
+        $username = $this->input("email");
+        $password = $this->input("password");
+        return ["email" => $username, "password" => $password];
+    }
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
