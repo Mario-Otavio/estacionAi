8ee1b1c37b5a223ee258c7f65f50d7ee3b6a7900
MuriloMafra <murilommafra@gmail.com>
2023-08-01 11:20:07 -0400

    Edições do show
    Adicionado o show de veículos

--- resources/views/veiculos/veiculo_show.blade.php
+++ resources/views/veiculos/veiculo_show.blade.php
@@ -7,27 +7,28 @@
 </head>
 <body>
 @include('layouts.navbar')
+
+<section>
 <div class="container">
-<table class="table table-borderless">
-    <thead>
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Placa</th>
-            </tr>
-    </thead>
-    
-    <tr>
-    <th scope="row"> {{ $veiculo->id }} </th>
-     <td> {{ $veiculo->placa }} </td>
-     <td colspan="2">
-        <form action="POST">
-            
-        </form>
-     </td>
-    </tr>
-    
-    </table>   
+<div class="container-sm px-4 text-center">
+<div class="card" style="width: 18rem;margin: 100 auto;">
+  <!-- <img src="..." class="card-img-top" alt="..."> -->
+  <div class="card-body">
+    <h5 class="card-title">Detalhes Veiculo {{$veiculo->placa}}</h5>
+    <ul class="list-group">
+        <li class="list-group-item"><b>ID:</b> {{$veiculo->id}}</li>
+        <li class="list-group-item"><b>Placa do veiculo:</b> {{$veiculo->placa}}</li>
+        <li class="list-group-item"><b>Marca do veiculo:</b> {{$veiculo->marca}}</li>
+        <li class="list-group-item"><b>Modelo do veiculo:</b> {{$veiculo->modelo}}</li>
+        <li class="list-group-item"><b>Entrada: </b>{{$veiculo->created_at}}</li>
+        <li class="list-group-item"><b>Entrada: </b>{{$veiculo->updated_at}}</li>
+    </ul>
+    <a href="/dashboard" class="btn btn-primary">Voltar para iniciar</a>
+  </div>
 </div>
+</div>
+</div>
+</section>
 </body>
 
 </html>
